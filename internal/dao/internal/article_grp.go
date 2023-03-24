b970c3d33f933289c8f9a6b3227fb1b861d982a2
王仁义 <tyyn1022@163.com>
2023-03-24 18:43:05 +0800

    0.3.0 - 升级框架版本

--- internal/dao/internal/article_grp.go
+++ internal/dao/internal/article_grp.go
@@ -76,6 +76,6 @@ func (dao *ArticleGrpDao) Ctx(ctx context.Context) *gdb.Model {
 //
 // Note that, you should not Commit or Rollback the transaction in function f
 // as it is automatically handled by this function.
-func (dao *ArticleGrpDao) Transaction(ctx context.Context, f func(ctx context.Context, tx *gdb.TX) error) (err error) {
+func (dao *ArticleGrpDao) Transaction(ctx context.Context, f func(ctx context.Context, tx gdb.TX) error) (err error) {
 	return dao.Ctx(ctx).Transaction(ctx, f)
 }
