b72fb3f1f5adeaa7ac58d69045999f7df8526ceb
oldme <tyyn1022@gmail.com>
2024-11-22 16:29:47 +0800

    feat: v1.5.0

--- main.go
+++ main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	_ "github.com/oldme-git/oldme-api/internal/logic"
-
 	"errors"
 	"fmt"
 
-	_ "github.com/gogf/gf/contrib/drivers/mysql/v2"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/gctx"
 	"github.com/gogf/gf/v2/os/gtime"
 	"github.com/oldme-git/oldme-api/internal/cmd"
+
+	_ "github.com/gogf/gf/contrib/drivers/mysql/v2"
+	_ "github.com/oldme-git/oldme-api/internal/logic"
 	_ "github.com/oldme-git/oldme-api/internal/utility"
 )
 
 const (
-	VERSION = "v1.4.1"
+	VERSION = "v1.5.0"
 )
 
 func main() {
